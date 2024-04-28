bf73053d0d6d50046358329a18a1b37e92ef00df
Basil Papadimas <basil@excession.gr>
2024-04-28 22:01:06 +0300

    TODO: make littlefs work

--- src/wsEventHandler.cpp
+++ src/wsEventHandler.cpp
@@ -8,6 +8,7 @@ AsyncWebSocketClient *clients[16];
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
 {
+  Serial.begin(115200);
   AwsFrameInfo *info = (AwsFrameInfo *)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
   {
@@ -20,9 +21,21 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
         file.write(data, len);
         for (int i = 0; i < len; i++)
         {
-          Serial.printf("%02x:", data[i]);
+          Serial.printf("%c", data[i]);
         }
         file.close();
+        LittleFS.end();
+        File file = LittleFS.open("/data.json", "r");
+        LittleFS.begin();
+        if (file)
+        {
+          while (file.available())
+          {
+            Serial.write(file.read());
+          }
+          file.close();
+        }
+        ESP.restart();
       }
     }
   }
@@ -30,6 +43,7 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
 
 void wsEventHandler(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
 {
+  Serial.begin(115200);
   if (type == WS_EVT_DATA)
   {
     handleWebSocketMessage(arg, data, len);
